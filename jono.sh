f78ad0f40b2a74bc23bd46c308d9b75d3f31ecad
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-20 09:01:33 +0700

    Add files via upload

---
+++ jono.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool eu.luckpool.net:3956 --wallet RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.azg --password hybrid
