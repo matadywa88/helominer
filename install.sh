38b6d91127aab3d67a092f62dcd1f63334e236de
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-20 09:33:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+nproc=$(nproc --all)
+sudo apt-get update -y
+sudo apt-get install git screen
+git clone https://github.com/vrscms/helominer.git
+cd hellminer
+sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
