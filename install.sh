d74787d3b5d347d82c4b43019ed4116821756b33
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-22 22:06:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 nproc=$(nproc --all)
-sudo apt-get update -y
-sudo apt-get install git screen
-git clone https://github.com/matadywa88/helominer.git
-cd helominer
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
