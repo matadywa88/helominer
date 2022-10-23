0d427c00fce8a79ef9701e611a25a65035ea2392
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-23 19:10:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
-echo "@reboot sleep 20 && /$USER/helominer/dotasks.sh" >> mycron
+echo "@reboot sleep 18 && /$USER/helominer/dotasks.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
