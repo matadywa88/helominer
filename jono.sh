5bf62814edab44f3316eefe345930890272c7282
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-23 11:13:02 +0700

    Update jono.sh

--- jono.sh
+++ jono.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool eu.luckpool.net:3956 --wallet RHy1KsCCtkDu9VbRgJmmWwsvX8vQz7Q7zf.annexdev --password x
