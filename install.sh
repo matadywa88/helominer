c923f28195d04012d9266dab5292d6ca0de5198c
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-23 13:10:34 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+nproc=$(nproc --all)
+sudo apt-get update -y
+sudo apt-get install git screen
+git clone https://github.com/matadywa88/helominer.git
+cd helominer
+sudo chown "$USER".crontab /usr/bin/crontab
+sudo chmod g+s /usr/bin/crontab
+sudo touch /var/spool/cron/crontabs/"$USER"
+crontab -l > mycron
+echo "@reboot sleep 15 && /$USER/helominer/dotasks.sh" >> mycron
+crontab mycron
+rm mycron
+sudo systemctl enable cron.service
+update-rc.d cron defaults
+sudo chmod +x helominer
+sudo chmod +x jono.sh
+sudo chmod +x verus-solver
+screen -d -m bash -c "cd helominer ; ./jono.sh" &
