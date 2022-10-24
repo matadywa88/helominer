4679a0585bad3f49c77edfab28abce2685d5877c
matadywa88 <116230235+matadywa88@users.noreply.github.com>
2022-10-24 23:18:17 +0700

    Update jono.sh

--- jono.sh
+++ jono.sh
@@ -1,1 +1,1 @@
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3957 --wallet RXMf3LJNWsFQgb27kFPETVNdu2YEgmtDyy.awnex6 --password x
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3957 --wallet RXMf3LJNWsFQgb27kFPETVNdu2YEgmtDyy.agoqi --password x
